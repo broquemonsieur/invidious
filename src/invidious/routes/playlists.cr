f165b9dde37638b2c2fea41368066dd0ab14604a
broquemonsieur <bracero-uplink.08@icloud.com>
2023-06-27 00:25:01 -0700

    getcompilation route to routes that are registered

--- src/invidious/routes/playlists.cr
+++ src/invidious/routes/playlists.cr
@@ -18,6 +18,8 @@ module Invidious::Routes::Playlists
   end
 
   def self.create(env)
+    LOGGER.info("3. create")
+    LOGGER.info("creating a play")
     locale = env.get("preferences").as(Preferences).locale
 
     user = env.get? "user"
@@ -381,6 +383,8 @@ module Invidious::Routes::Playlists
   end
 
   def self.show(env)
+    LOGGER.info("4. show")
+    LOGGER.info("showing a play")
     locale = env.get("preferences").as(Preferences).locale
 
     user = env.get?("user").try &.as(User)
